23f56b43bd06450e7df8c4ed452bf8586c5bbcba
frankcpa <frankcpa84@gmail.com>
2025-09-02 08:05:30 -0400

    atualizando aplicação

--- src/main/java/carregabanco/controller/AlunoControllerAPi.java
+++ src/main/java/carregabanco/controller/AlunoControllerAPi.java
@@ -3,8 +3,6 @@ package carregabanco.controller;
 import java.util.List;
 import java.util.Optional;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +22,10 @@ import carregabanco.service.AlunoService;
 import carregabanco.service.CarregarBancoService;
 
 import javax.validation.Valid;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/alunos")
 public class AlunoControllerAPi {
-
 
     @Autowired
     private AlunoService AlunoService;
@@ -37,102 +33,90 @@ public class AlunoControllerAPi {
     @Autowired
     private CarregarBancoService carregarBancoService;
 
-
-//Criar a função de buscar todos;
+    // Criar a função de buscar todos;
     @GetMapping("/todos")
-public ResponseEntity<List<AlunoModel>> buscarTodos() {
-    List<AlunoModel> alunos = AlunoService.buscarTodosAlunos();
-    
-    if (alunos.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<List<AlunoModel>> buscarTodos() {
+        List<AlunoModel> alunos = AlunoService.buscarTodosAlunos();
+
+        if (alunos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(alunos, HttpStatus.OK);
     }
 
-    return new ResponseEntity<>(alunos, HttpStatus.OK);
-    }
+    // Criar a função de inserir;
 
-  //Criar a função de inserir;
-
-   @PostMapping("/inserir")
-    public ResponseEntity<AlunoModel> inserirAluno( @Valid @RequestBody AlunoModel aluno) {
+    @PostMapping("/inserir")
+    public ResponseEntity<AlunoModel> inserirAluno(@Valid @RequestBody AlunoModel aluno) {
         AlunoModel salvo = AlunoService.inserir(aluno);
         return new ResponseEntity<>(salvo, HttpStatus.CREATED);
     }
 
-    //Criar a função de excluir;
-
+    // Criar a função de excluir;
 
     @DeleteMapping("/deletar/{id}")
-public ResponseEntity<Void> excluirAluno(@PathVariable Long id) {
-    boolean excluido = AlunoService.excluir(id);
-    
-    if (excluido) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } else {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
+    public ResponseEntity<Void> excluirAluno(@PathVariable Long id) {
+        boolean excluido = AlunoService.excluir(id);
+
+        if (excluido) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    //Criar três maneiras diferentes de buscar (por nome, curso e outra);
-
+    // Criar três maneiras diferentes de buscar (por nome, curso e outra);
 
     @GetMapping("/buscar/{nome}")
-public ResponseEntity<List<AlunoModel>> buscarPorNome(@PathVariable String nome) {
-    List<AlunoModel> alunos = AlunoService.buscarPorNome(nome);
+    public ResponseEntity<List<AlunoModel>> buscarPorNome(@PathVariable String nome) {
+        List<AlunoModel> alunos = AlunoService.buscarPorNome(nome);
 
-    if (alunos.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        if (alunos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(alunos, HttpStatus.OK);
     }
 
-    return new ResponseEntity<>(alunos, HttpStatus.OK);
-}
+    @GetMapping("/buscar/curso/{curso}")
+    public ResponseEntity<List<AlunoModel>> buscarPorCurso(@PathVariable String curso) {
+        List<AlunoModel> alunos = AlunoService.buscarPorCurso(curso);
 
-@GetMapping("/buscar/curso/{curso}")
-public ResponseEntity<List<AlunoModel>> buscarPorCurso(@PathVariable String curso) {
-    List<AlunoModel> alunos = AlunoService.buscarPorCurso(curso);
+        if (alunos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
-    if (alunos.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(alunos, HttpStatus.OK);
     }
 
-    return new ResponseEntity<>(alunos, HttpStatus.OK);
-}
+    @GetMapping("/buscar/campus/{campus}")
+    public ResponseEntity<List<AlunoModel>> buscarPorCampus(@PathVariable String campus) {
+        List<AlunoModel> alunos = AlunoService.buscarPorCampus(campus);
 
+        if (alunos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
-@GetMapping("/buscar/campus/{campus}")
-public ResponseEntity<List<AlunoModel>> buscarPorCampus(@PathVariable String campus) {
-    List<AlunoModel> alunos = AlunoService.buscarPorCampus(campus);
-
-    if (alunos.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(alunos, HttpStatus.OK);
     }
 
-    return new ResponseEntity<>(alunos, HttpStatus.OK);
-}
+    @PutMapping("/alunos/editar/{id}")
+    public ResponseEntity<AlunoModel> atualizarAluno(@PathVariable Long id, @RequestBody AlunoModel novoAluno) {
+        Optional<AlunoModel> existente = AlunoService.buscarAlunoId(id);
 
-
-
-@PutMapping("/alunos/editar/{id}")
-public ResponseEntity<AlunoModel> atualizarAluno(@PathVariable Long id, @RequestBody AlunoModel novoAluno) {
-    Optional<AlunoModel> existente = AlunoService.buscarAlunoId(id);
-
-    if (existente.isPresent()) {
-        AlunoModel atualizado = AlunoService.atualizar(id, novoAluno);
-        return new ResponseEntity<>(atualizado, HttpStatus.OK);
-    } else {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (existente.isPresent()) {
+            AlunoModel atualizado = AlunoService.atualizar(id, novoAluno);
+            return new ResponseEntity<>(atualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-}
 
-
-@PostMapping("/upload")
-public ResponseEntity<String> uploadArquivo(@RequestParam("arquivo") MultipartFile arquivo) {
-    String resultado = carregarBancoService.carregarAlunosDoArquivo(arquivo);
-    return ResponseEntity.ok(resultado);
-}
-
-
-
-
-
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadArquivo(@RequestParam("arquivo") MultipartFile arquivo) {
+        String resultado = carregarBancoService.carregarAlunosDoArquivo(arquivo);
+        return ResponseEntity.ok(resultado);
+    }
 
 }
