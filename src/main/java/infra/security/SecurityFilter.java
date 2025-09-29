f824cfd12d532baab334137b20f1f6faf4bcb5b7
Carlos <carloswelliton7@gmail.com>
2025-09-29 16:19:46 -0400

    atualização nas configurações do Spring Security, passando o Filter para que apenas quem esteja logado possa realizado o login, e por meio dele transitar o token de autenticação para usar as rotas protegidas

--- src/main/java/infra/security/SecurityFilter.java
+++ src/main/java/infra/security/SecurityFilter.java
@@ -3,10 +3,13 @@ package infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import funcionarios.repository.FuncionarioRepository;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,15 +22,38 @@ public class SecurityFilter extends OncePerRequestFilter{
   private TokenService tokenService;
 
   @Autowired
-  private FuncionarioRepository funcionarios;
+  private FuncionarioRepository repository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-      
-    
+        var tokenJWT = recuperarToken(request);
+
+        try {
+          if (tokenJWT != null) {
+              var subject = tokenService.getSubject(tokenJWT);
+              var usuario = repository.findByUsername(subject);
+              var authentication = new UsernamePasswordAuthenticationToken(
+                      usuario, null, usuario.getAuthorities()
+              );
+              SecurityContextHolder.getContext().setAuthentication(authentication);
+          }
+          filterChain.doFilter(request, response);
+        } catch (RuntimeException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Token inválido ou expirado");
+          }
+
   }
 
+
+  private String recuperarToken(HttpServletRequest request){
+    var autorizationHeader = request.getHeader("Authorization");
+    if(autorizationHeader != null){
+      return autorizationHeader.replace("Bearer ","");
+    }
+    return null;
+  }
   
   
 
